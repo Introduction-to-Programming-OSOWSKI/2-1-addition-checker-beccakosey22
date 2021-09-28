f02ece5f81d705fcc05e3533b6bb25a82b182468
beccakosey22 <820512@mystma.org>
2021-09-28 08:58:03 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE HERE
+#Create a function addCheck() That returns true/false if the sum given variables x and y correctly add up to z.
+def addCheck(x, y, z):
+    if x + y == z:
+        return "true"
+    else:
+        return "false"
+print(addCheck(6, 4, 10))
